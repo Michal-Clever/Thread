513aac24a630718e48ed4ce19563bc94fe240db9
Michal-Clever <inz.michal.milczarek@gmail.com>
2018-08-05 20:16:04 +0200

    Thread

---
+++ src/OldThreadTest.java
@@ -0,0 +1,35 @@
+public class OldThreadTest {
+
+    public static void main(String[] args) {
+
+
+
+
+        Runnable someTask = new Runnable() {  // interfejs runnable
+            @Override
+            public void run() {
+                for (int i = 0; i < 5000; i++) {
+                    System.out.println("Witaj,jestem z wątku numer 1");
+                    Thread.yield();
+                }
+            }
+        };
+
+        Thread thread = new Thread(someTask);
+        thread.start();
+        for (int i = 0; i < 5000; i++) {
+
+            System.out.println("Witaj, jestem z wątku numer 0");
+            Thread.yield();    // mała optymalizacja
+        }
+
+
+
+
+
+
+
+
+    }
+
+}
---
+++ src/SomeError.java
@@ -0,0 +1,45 @@
+import java.util.Vector;
+
+public class SomeError {
+    public static void main(String[] args) {
+        Value age = new Value();
+
+
+        for (int i = 0; i < 5000; i++) {
+            new SomeThreadTask(age).start();
+            System.out.println(age.getAge());
+        }
+    }
+
+    public static class Value{
+        private int age;
+
+        public  int getAge() {
+            return age;
+        }
+
+        public synchronized void action() {
+            age++;
+            age--;
+        }
+    }
+
+    public static class SomeThreadTask extends Thread {
+        private Value age;
+        public SomeThreadTask(Value age) {
+            this.age = age;
+        }
+
+        @Override
+        public  void run() {
+            makeAction();
+        }
+
+        private  void makeAction() {
+            for (int i = 0; i < 5000; i++) {
+                age.action();
+            }
+        }
+    }
+
+}
